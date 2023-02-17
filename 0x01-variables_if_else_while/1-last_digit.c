f74adf3d9530797b79c10f6420da3763202f7b40
olaide011 <ojuolapeolaide92@gmail.com>
2023-02-17 03:53:30 -0800

    1-last_digit.c (using the boolen operators)

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - Getting the last digit
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+	int l_d;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	l_d = n % 10;
+	if (l_d > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, l_d);
+	} else if (l_d == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, l_d);
+	} else if (l_d < 6 && l_d != 0)
+	{
+		printf"(Last digit of %d is %d and is less than 6 and not 0\n", n, l_d);
+	}
+	return (0);
+}
