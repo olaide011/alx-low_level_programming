cf2c094ce6702c2d0571057834360fb78ef461aa
olaide011 <ojuolapeolaide92@gmail.com>
2023-02-17 05:33:30 -0800

    7-print_tebahpla.c decrement

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - printing z-a
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char alp = 'z';
+
+	while (alp <= 'a')
+	{
+		putchar(alp);
+		alp--;
+	}
+	putchar('\n');
+	return (0);
+}
