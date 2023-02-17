a63ebd5989e0c2f130f82d5ce6b7c76452ead8a9
olaide011 <ojuolapeolaide92@gmail.com>
2023-02-17 05:08:46 -0800

    5-print_numbers.c (using while loop)

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - printing 1 -10
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i <= 10)
+	{
+		printf("%d", i);
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
+
