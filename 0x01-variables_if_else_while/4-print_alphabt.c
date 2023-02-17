60a76ada0deac9be2245a3124e37d3653b895c15
olaide011 <ojuolapeolaide92@gmail.com>
2023-02-17 04:51:36 -0800

    4-print_alphabt.c (using while loop and if conditionals)

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - print alphabet - q,e.
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char abc = 'a';
+
+	while (abc <= 'z')
+	{
+		if (abc != 'q' && abc != 'e')
+		{
+			putchar(abc);
+
+		}
+		abc++;
+	}
+	putchar('\n');
+	return (0);
+
