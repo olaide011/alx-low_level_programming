59c300bbe99d21e76d5f5927d482048a57afcda9
olaide011 <ojuolapeolaide92@gmail.com>
2023-02-21 10:32:39 -0800

    Write a function that prints the last digit of a number.

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs - return the absolute value of a given integer
+ * @i: the unt value to check
+ * Return: the absolute value of int
+ */
+int _abs(int i)
+{
+	if (i >= 0)
+	{
+		return (i);
+	}
+	return (-i);
+}
