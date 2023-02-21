43ace7bb7d70a53a8520f245a8a41dc5d01ce7e8
olaide011 <ojuolapeolaide92@gmail.com>
2023-02-21 06:10:33 -0800

    Write a program that prints _putchar, followed by a new line

--- 0x02-functions_nested_loops/_putchar.c
+++ 0x02-functions_nested_loops/_putchar.c
@@ -1,13 +1,14 @@
-#include "main"
-#include <stdio.h>
+#include "main.h"
+#include <unistd.h>
+
 /**
- * _putchar - writes the character c to the standard-output
- * c: character to print
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
  *
- * Return: on sucess 1
- * on error, - 1 will be returned and error
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c)
 {
-	return(write(1,&c,1));
+	return (write(1, &c, 1));
 }
