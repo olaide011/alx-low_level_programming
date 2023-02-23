24d2ec957ade26695b839be11e777ecdf0a8c7d2
olaide011 <ojuolapeolaide92@gmail.com>
2023-02-23 05:05:49 -0800

    Prototype: int mul(int a, int b)

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "maih.h"
+#include <stdio.h>
+
+/**
+ * mul - multiplies two integers
+ * @a: first integer
+ * @b: second integer
+ * Return: the addition of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
