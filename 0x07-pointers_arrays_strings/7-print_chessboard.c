8bc772208346fc72757e2ba4f58fa6931cef66fa
olaide011 <ojuolapeolaide92@gmail.com>
2023-03-06 09:17:17 -0800

    a function that prints the chessboard.

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "maon.h"
+
+/**
+ * print_chessboard - prints the chessboard
+ * @a: pointer to pieces to print
+ *
+ * Return: void
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	unsigned int i, b;
+
+	for (i = 0 ; i < 8 ; i++)
+	{
+		for (b = 0 ; b < 8 ; b++)
+		{
+			_putchar(*((*a + i * 8) + b));
+		}
+		_putchar('\n');
+	}
+}
