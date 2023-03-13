221fe54833aa28994909adec3dbae093d41bca50
olaide011 <ojuolapeolaide92@gmail.com>
2023-03-13 03:38:28 -0700

    update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,13 +1,39 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
 
 /**
  * main - function
  * @argc: argument count
  * @argv: argument
  *
- * Return: int
+ * Return: 0
  */
-int main(int argc, char *argv)
+int main(int argc, char *argv[])
 {
+	int i, c, sum = 0;
+
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (i = 1; i < argc; i++)
+	{
+		for (c = 0; argv[i][c] != '\0'; c++)
+		{
+			if (!isdigit(argv[i][c]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[i]);
+	}
+
+	printf("%d\n", sum);
+
+
 	return (0);
 }
