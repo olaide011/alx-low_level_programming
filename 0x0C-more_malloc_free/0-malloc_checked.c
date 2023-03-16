a1a4959c155167f144af7fef498981b5a366e89e
olaide011 <ojuolapeolaide92@gmail.com>
2023-03-16 10:48:06 -0700

    0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,5 +1,6 @@
 #include "main.h"
 
+
 /**
  * malloc_checked - cause normal process termination with a status value of 98
  *
@@ -13,9 +14,9 @@ void *malloc_checked(unsigned int b)
 
 	i = malloc(b);
 	if (i == NULL)
-	{	
+
 		exit(98);
-	}
+
 	return (i);
 
 }
