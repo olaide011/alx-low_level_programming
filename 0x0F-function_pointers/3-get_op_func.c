0d017300e978147e68bda526c414d07b5b7cdd8a
olaide011 <ojuolapeolaide92@gmail.com>
2023-03-22 12:37:51 -0700

    0x0F-function_pointers

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -20,8 +20,13 @@ int (*get_op_func(char *s))(int, int)
 	};
 	int i = 0;
 
-	while (ops[i].op != NULL && *(ops[i].op) != *s)
+	while (ops[i].op != NULL && s[1] == '\0')
+	{
+		if (ops[i].op[0] == *s)
+			return ((ops[i].f));
 		i++;
+	}
 
-	return (ops[i].f);
+	return (NULL);
+
 }
