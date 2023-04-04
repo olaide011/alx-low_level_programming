06b7797eb8b8792fff134155f66cb0fc12066931
olaide011 <ojuolapeolaide92@gmail.com>
2023-04-03 17:31:20 -0700

    0x13-more_singly_linked_lists

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_listint- function to print the element
+ *
+ * @h: pointer representing head
+ *
+ * Return: counter
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t i;
+
+	while (h != NULL)
+	{
+		i++;
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (i);
+}
